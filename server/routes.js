bd334b9ae3d7fa833ce66f75bb79d8f57c609d1b
dong yuangui <513962116@qq.com>
2016-07-10 21:40:35 +0800

    debug form api

--- server/routes.js
+++ server/routes.js
@@ -15,7 +15,7 @@ export default function(app) {
   app.use('/auth', require('./auth'));
 
   app.use('/page/index', require('./api/html'));
-
+  app.use('/html', require('./api/html'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
